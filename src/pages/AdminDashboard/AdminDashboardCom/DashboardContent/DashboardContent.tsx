6880f57c0c392cf00df5c42255c59c0ed5a4e7f4
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-09-15 09:22:25 +0330

    Create DashboardContent component

---
+++ src/pages/AdminDashboard/AdminDashboardCom/DashboardContent/DashboardContent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DashboardContent.css";
+
+function DashboardContent() {
+  return (
+    <div className="dashboard-content">
+      <h1>Welcome to GoFood Admin Panel</h1>
+    </div>
+  );
+}
+
+export default DashboardContent;
