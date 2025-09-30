c046d7784ab47fda23f4e1e84d5be4b480179f13
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-09-30 09:54:36 +0330

    Create OrderPage component

---
+++ src/pages/OrderPage/OrderPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OrderSection from "../OrderSection/OrderSection";
+
+const OrderPage: React.FC = () => {
+  return (
+    <div style={{padding:"1rem"}} className="order-page">
+      <h1>Orders Page</h1>
+      <OrderSection />
+    </div>
+  );
+};
+
+export default OrderPage;
