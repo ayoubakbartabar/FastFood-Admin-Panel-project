f11d0dce96a5f20c7c5fe3bbde039e0e386ae7da
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-10-04 08:58:01 +0330

    Create HomePaginationPage component

---
+++ src/pages/HomePaginationPage/HomePaginationPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+import './HomePaginationPage.css'
+
+
+function HomePaginationPage() {
+  return (
+    <div>HomePaginationPage</div>
+  )
+}
+
+export default HomePaginationPage
